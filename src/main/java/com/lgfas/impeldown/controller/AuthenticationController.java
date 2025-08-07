2db44b509ca563445b0ebdb32198113ea5d7886b
lgfas <luis.guilherme2811@gmail.com>
2025-08-07 12:07:50 -0300

    feat: início da implementação do spring security

---
+++ src/main/java/com/lgfas/impeldown/controller/AuthenticationController.java
@@ -0,0 +1,14 @@
+package com.lgfas.impeldown.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    @GetMapping
+    public String login() {
+        return "Welcome";
+    }
+
+}
