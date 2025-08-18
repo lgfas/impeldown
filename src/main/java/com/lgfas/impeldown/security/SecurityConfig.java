e9e0b9a90246d7c36b2c95e2fc0d1a662267ba22
lgfas <luis.guilherme2811@gmail.com>
2025-08-18 12:01:07 -0300

    feat: adicionando usuarios em memória e encriptando suas senhas

--- src/main/java/com/lgfas/impeldown/security/SecurityConfig.java
+++ src/main/java/com/lgfas/impeldown/security/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -22,5 +27,18 @@ public class SecurityConfig {
                 )
                 .httpBasic(hb -> {})
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("ADMINISTRADOR").build(),
+                User.withUsername("guarda").password(passwordEncoder.encode("1234")).roles("GUARDA").build()
+        );
     }
 }
