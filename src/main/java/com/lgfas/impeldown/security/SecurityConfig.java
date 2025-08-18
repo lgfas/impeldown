4b42c894335253c4b87f66dc82343954cee84df9
lgfas <luis.guilherme2811@gmail.com>
2025-08-18 11:49:57 -0300

    feat: implementação do spring security para poder utilizar Basic Auth no Postman

---
+++ src/main/java/com/lgfas/impeldown/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.lgfas.impeldown.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(hb -> {})
+                .build();
+    }
+}
